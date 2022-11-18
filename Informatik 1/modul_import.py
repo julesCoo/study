091e71adc0e24ff8f41860bb957e0304a9b8f986
Daniel <daniel@pothos.dev>
2022-11-18 15:26:47 +0100

    typo

--- Informatik 1/modul_import.py
+++ Informatik 1/modul_import.py
@@ -1,7 +1,7 @@
 """
-Schreiben Sie ein weiteres Python-Script, in dem Sie das zuvor erstellte Script(funktionen.py) als Python-Modul verwenden.
+Schreiben Sie ein weiteres Python-Script, in dem Sie das zuvor erstellte Script (funktionen.py) als Python-Modul verwenden.
 
-Beim Import des Moduls soll vorerst noch keine Berechnung erfolgen (ähnlich wiebeim Import von „math“ wo auch noch nichts passiert).
+Beim Import des Moduls soll vorerst noch keine Berechnung erfolgen (ähnlich wie beim Import von „math“ wo auch noch nichts passiert).
 Erst nach dem Import soll zumindest eine der implementierten Funktionen aufgerufen werden.
 """
 
