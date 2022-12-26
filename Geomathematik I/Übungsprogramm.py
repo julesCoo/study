4552ba4d21fe3b723180d8c476db8b97b4c9ed9a
Daniel Ebert <daniel@pothos.dev>
2022-12-26 13:21:35 +0100

    fix RHS

--- Geomathematik I/Übungsprogramm.py
+++ Geomathematik I/Übungsprogramm.py
@@ -57,31 +57,36 @@ rotation_axis = h1.cross(h2).normalized()
 # Since the two measurement stations might not be inside the same rotational plane (e.g.
 # they are on different heights along the rotational axis), we have to calculate different
 # centers for each point.
-center1 = P1.projected_on(rotation_axis)
-center2 = P2.projected_on(rotation_axis)
+c1 = P1.projected_on(rotation_axis)
+c2 = P2.projected_on(rotation_axis)
 
 # Also project the rotated points onto the axis for scrutiny. They should be identical to the
 # center of the non-rotated point!
-center1_ = Q1.projected_on(rotation_axis)
-center2_ = Q2.projected_on(rotation_axis)
-assert (center1 - center1_).length() < 1e-6
-assert (center2 - center2_).length() < 1e-6
+c1_ = Q1.projected_on(rotation_axis)
+c2_ = Q2.projected_on(rotation_axis)
+assert (c1 - c1_).length() < 1e-6
+assert (c2 - c2_).length() < 1e-6
 
 # Using two vectors going from the center to the two point position (original and reported),
 # we can find the angle of rotation as the angle between these two vectors.
-cp1 = P1 - center1
-cp1_ = Q1 - center1
+cp1 = P1 - c1
+cp1_ = Q1 - c1
 angle1 = cp1.angle_between(cp1_)
 
 # To check that the rotation is the same for both points (which we expect if the rotating body
 # is rigid), calculate the angle for the second point and compare.
-cp2 = P2 - center2
-cp2_ = Q2 - center2
+cp2 = P2 - c2
+cp2_ = Q2 - c2
 angle2 = cp2.angle_between(cp2_)
 assert abs(angle1 - angle2) < 1e-6
 
 # Angles deviate by about 1%. Calculate the average and work with that to minimize the error.
 angle = (angle1 + angle2) / 2
+
+# Check and potentially correct the orientation of the rotation axis
+right_system = rotation_axis.cross(cp1).dot(cp1_) > 0
+if not right_system:
+    rotation_axis = -rotation_axis
 
 # Now that we know the rotation over the span of 5 years, we can extrapolate it into the future.
 angle_a = angle / 5
@@ -103,7 +108,7 @@ P3_lon_delta = Q3_lon - P3_lon
 
 """
 
-Backward Rotation
+Backward Rotation (for verification)
 
 """
 
@@ -112,7 +117,7 @@ Q1_ = R5 * P1
 Q2_ = R5 * P2
 assert Q1.equals(Q1_)
 assert Q2.equals(Q2_)
- 
+
 
 Q1_lat_, Q1_lon_, _ = SphereCoords.from_vec3(Q1_)
 Q2_lat_, Q2_lon_, _ = SphereCoords.from_vec3(Q2_)
@@ -122,8 +127,14 @@ P1_lon_delta_ = Q1_lon_ - P1_lon
 P2_lat_delta_ = Q2_lat_ - P2_lat
 P2_lon_delta_ = Q2_lon_ - P2_lon
 
+"""
+
+Output
+
+"""
+
 # print(f"rotational axis: {rotation_axis}")
 # print(f"angle of rotation: {fmt_deg_str(angle)}")
 
-# print(f"db (Columbus, OH): {fmt_deg_str(P3_lat_delta, precision=5)}")
-# print(f"dl (Columbus, OH): {fmt_deg_str(P3_lon_delta, precision=5)}")
+print(f"db (Columbus, OH): {fmt_deg_str(P3_lat_delta, precision=5)}")
+print(f"dl (Columbus, OH): {fmt_deg_str(P3_lon_delta, precision=5)}")
