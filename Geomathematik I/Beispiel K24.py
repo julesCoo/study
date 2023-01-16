ef37c92918d11e49ce425f2af86063b00aed11a0
Daniel <daniel@pothos.dev>
2023-01-16 09:46:17 +0100

    ...

--- Geomathematik I/Beispiel K24.py
+++ Geomathematik I/Beispiel K24.py
@@ -14,23 +14,19 @@ P2 = SphereCoords(
     lam=from_deg(-30, 26, 4),
 )
 
-s21, a21 = ha2(P2, P1)
+s21, a21, _ = ha2(P2, P1)
 
 s13 = 10500  # km
 a23 = from_deg(31, 24, 12)
 
-# TODO Nur eine gültige Lösung!!
-T, T_ = SphereTriangle.ssw(
-    alpha=tau - a21 + a23,
+[T] = SphereTriangle.ssw(
+    alpha=a21 - a23,
     a=s13 / radius,
     c=s21,
 )
 
 P3, _ = ha1(P2, T.b, a23)
-P3_, _ = ha1(P2, T_.b, a23)
 
 area = SphereTriangle.ppp(P1, P2, P3).area(radius)
-area_ = SphereTriangle.ppp(P1, P2, P3_).area(radius)
 
 print(f"{P3} ({area:,} km²)")
-print(f"{P3_} ({area_:,} km²)")
